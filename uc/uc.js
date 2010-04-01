a5aa1deaeb07da77d87e89c6b849867f0b118f33
satyr <murky.satyr@gmail.com>
2010-04-01 17:52:55 +0900

    menuitem

--- uc/uc.js
+++ uc/uc.js
@@ -3,10 +3,18 @@
 UC = userChrome = {
   __proto__: Cu.import('resource://uc/uc.jsm', null).UC,
 };
-if(location.href === UC.URL_MAIN && UC.prefs.get('extensions.uc.sidebar')){
-  document.getElementById('sidebar').addEventListener(
-    'load', function UC_onSidebar(){
-      if(this.currentURI.scheme === 'chrome') UC.load(this.contentWindow);
-    }, true);
-}
+
 UC.load(self);
+
+if(location.href === UC.URL_MAIN){
+  if(UC.prefs.get('extensions.uc.sidebar'))
+    document.getElementById('sidebar').addEventListener(
+      'load', function UC_onSidebar(){
+        if(this.currentURI.scheme === 'chrome') UC.load(this.contentWindow);
+      }, true);
+  let mi = document.createElement('menuitem');
+  mi.setAttribute('label', 'uc');
+  mi.setAttribute('accesskey', 'u');
+  mi.setAttribute('oncommand', 'UC.options()');
+  document.getElementById('menu_ToolsPopup').appendChild(mi);
+}

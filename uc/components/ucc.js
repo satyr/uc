ecb04d803260a0764809acf8d635b814d91c5de3
satyr <murky.satyr@gmail.com>
2010-06-24 01:11:18 +0900

    ucc: app-startup -> profile-after-change

--- uc/components/ucc.js
+++ uc/components/ucc.js
@@ -3,24 +3,17 @@ const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 NSGetModule = XPCOMUtils.generateNSGetModule([UCC]);
 
-function UCC(){}
+function UCC(){
+  Cu.import('resource://uc/uc.jsm');
+  UC.Observer.addObserver(this, 'domwindowopened', false);
+}
 UCC.prototype = {
   classDescription: 'uc loader',
   classID: Components.ID('{98c6d8bf-3eae-4e3f-8dc8-f91794e9782a}'),
   contractID: '@uc;1',
-  QueryInterface: XPCOMUtils.generateQI(
-    ['nsIObserver', 'nsIDOMEventListener', 'nsIFactory']),
-  _xpcom_categories:
-  [{category: 'app-startup', entry: 'm-uc', service: true}],
-
-  observe: function UCC_observe(subject, topic){ switch(topic){
-    case 'app-startup':
-    Cu.import('resource://uc/uc.jsm');
-    return UC.Observer.addObserver(this, 'final-ui-startup', false);
-    case 'final-ui-startup':
-    if(UC.AppInfo.inSafeMode) return;
-    return UC.Observer.addObserver(this, 'domwindowopened',  false);
-    case 'domwindowopened': subject.addEventListener('load', this, true);
-  }},
-  handleEvent: function UCC_handleEvent(ev) UC.init(ev.target.defaultView),
+  QueryInterface: XPCOMUtils.generateQI([]),
+  _xpcom_categories: [{category: 'profile-after-change'}],
+  observe: function UCC_onWindowOpen(win)
+    win && win.addEventListener('load', this, true),
+  handleEvent: function UCC_onDocumentLoad(ev) UC.init(ev.target.defaultView),
 };

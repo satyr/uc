733d21f54864d82ca984079948d969da8042f4ab
satyr <murky.satyr@gmail.com>
2010-07-03 10:19:06 +0900

    https://developer.mozilla.org/en/XPCOM/XPCOM_changes_in_Gecko_2.0

--- uc/components/ucc.js
+++ uc/components/ucc.js
@@ -1,19 +1,20 @@
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-
+const Cu = Components.utils;
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
-NSGetModule = XPCOMUtils.generateNSGetModule([UCC]);
 
 function UCC(){
   Cu.import('resource://uc/uc.jsm');
   UC.Observer.addObserver(this, 'domwindowopened', false);
 }
 UCC.prototype = {
-  classDescription: 'uc loader',
+  classDescription: 'uc boot',
   classID: Components.ID('{98c6d8bf-3eae-4e3f-8dc8-f91794e9782a}'),
-  contractID: '@uc;1',
+  contractID: '@u/c;1',
   QueryInterface: XPCOMUtils.generateQI([]),
   _xpcom_categories: [{category: 'profile-after-change'}],
   observe: function UCC_onWindowOpen(win)
     win && win.addEventListener('load', this, true),
   handleEvent: function UCC_onDocumentLoad(ev) UC.init(ev.target.defaultView),
 };
+for each(let x in ['Factory', 'Module']) try {
+  this['NSGet'+ x] = XPCOMUtils['generateNSGet'+ x]([UCC]);
+} catch([]){}
